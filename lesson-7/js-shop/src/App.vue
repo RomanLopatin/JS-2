cc1e951b2c152e435b6afb65b8edd9af7349c379
ROL <romanlopatin@gmail.com>
2022-01-18 19:00:04 +0300

    lesson-7

---
+++ lesson-7/js-shop/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="nav">
+    <router-view />
+    {{ showcase }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showcase: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/api/v1/showcase")
+      .then((res) => res.json())
+      .then((data) => {
+        this.showcase = data;
+      });
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
